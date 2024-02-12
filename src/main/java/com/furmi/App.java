e8c679ff64f67684e049562c1fd7fc83e4528ec6
furmi <147946906+Furmi37@users.noreply.github.com>
2024-02-12 19:17:25 +0100

    #5_teacher_operations

--- src/main/java/com/furmi/App.java
+++ src/main/java/com/furmi/App.java
@@ -3,6 +3,7 @@ package com.furmi;
 import com.furmi.model.Grades;
 import com.furmi.model.Student;
 import com.furmi.model.Teacher;
+import com.furmi.repository.GradesRespository;
 import com.furmi.repository.StudentRepository;
 import com.furmi.repository.TeacherRepository;
 import com.furmi.service.StudentService;
@@ -22,36 +23,38 @@ public class App {
 
         StudentRepository studentRepository = new StudentRepository(entityManager);
         StudentService studentService = new StudentService(studentRepository);
+        GradesRespository gradesRespository = new GradesRespository(entityManager);
 
         TeacherRepository teacherRepository = new TeacherRepository(entityManager);
-        TeacherService teacherService = new TeacherService(teacherRepository);
+        TeacherService teacherService = new TeacherService(teacherRepository,
+                studentRepository, gradesRespository);
 
 
 //        Teacher t = new Teacher(105,"Martin", "Schmitt", 6000, 5, "mgr","2024-01-01" );
-//        Student student1 = new Student(208,"Sus","Wright","2015-04-01","2b");
-//        Student student2= new Student(209,"Calvin","Klein","2015-08-19","2b");
+        Student s1 = new Student();
+        s1.setFirst_name("Mark");
+        s1.setLast_name("Godawski");
+        s1.setEmail("mark@gmail.com");
+        s1.setBirth_day("2015-04-09");
+        s1.setClass_name("2B");
+        s1.setStudent_id(4);
+        Student s2 = new Student(3, "Calvin", "Klein", "calvin@gmail.com", "2015-08-19", "2b");
 //        Subject subject = new Subject(5, "Computer Science", 105);
+////
+        teacherService.addGrade("calvin@gmail.com", 4, "2024-02-14", "English");
+        System.out.println("add grade -> calvin@gmail.com");
+//            studentService.showAllStudents();
+//        teacherService.createStudent(s2);
+//        System.out.println("Sus grades:");
+//        teacherService.showAllGradesForStudent("sus@gmail.com");
 
-//        Grades grade = new Grades(6,"Chemistry",209,4,"2024-02-09");
-//        teacherService.saveTeacher(t);
-        entityManager.getTransaction().begin();
+//        System.out.println("Matt grades:");
+//        teacherService.showAllGradesForStudent("matt@gmail.com");
 
-//        teacherService.deleteStudent(student2);
-//        Student byId = studentRepository.findById(208);
-//        studentRepository.updateInfo(student1);
-//        System.out.println(byId);
-//        List<Grades> allGradesForStudent = teacherRepository.getAllGradesForStudent(student1);
-//      s
-//        allGradesForStudent.forEach(System.out::println);
+//        studentService.showAllStudents();
 
-//        teacherRepository.addGrade(grade);
-//
-//        entityManager.getTransaction().begin();
-//        grade.setSubject1(subject);
-//        subject.setTeacher1(t);
-//        entityManager.getTransaction().commit();
-//        List<Grades> list = new ArrayList<>();
-//        list.add(grade);
+//        studentService.showAllStudents();
+
 
         entityManager.close();
         entityManagerFactory.close();
